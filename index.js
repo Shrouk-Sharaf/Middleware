8c6b7691db455ca1cd1cd958d4f8935d08c1b250
Shrouk-Sharaf <sayedahmedroka@gmail.com>
2025-05-10 10:19:19 +0300

    uploaded Task 1 of the session

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+// Middleware to log request details
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}` + new Date().toISOString());
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send('Session 17!')
+});
+
+app.listen(3000, () => {
+    console.log(`Server is running on port 3000`);
+});
